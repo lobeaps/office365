e7f333b2c55d70382295a1422ff8b0e5cbe56709
Philip Kjærsgaard - Lobe ApS <pk@lobe.dk>
2020-06-26 08:51:06 +0200

    Lobe's Fabric ui tema i version 2

--- FabricUI-lobe.ps1
+++ FabricUI-lobe.ps1
@@ -2,37 +2,37 @@ Install-Module -Name Microsoft.Online.SharePoint.PowerShell -force
 Connect-SPOService -Url https://lobedk-admin.sharepoint.com
 
 # https://developer.microsoft.com/en-us/fabric?fabricVer=5#/styles/themegenerator
+# https://fabricweb.z5.web.core.windows.net/pr-deploy-site/refs/heads/master/theming-designer/index.html
+
+
+# Fjern nuværende udgave af temaet
+Remove-SPOTheme -Name "Lobe V2"
+
 
 # Lobe light palette
 $themeColors = @{
     "themePrimary" = "#005a84";
-    "themeLighterAlt" = "#f0f7fa";
-    "themeLighter" = "#005a84";
-    "themeLight" = "#99c6da";
-    "themeTertiary" = "#4993b6";
-    "themeSecondary" = "#126a93";
-    "themeDarkAlt" = "#005277";
-    "themeDark" = "#004565";
-    "themeDarker" = "#00334a";
+    "themeLighterAlt" = "#000405"; # Gruppe titel (Moderne Intranet)
+    "themeLighter" = "#000e15"; # Menu bar (Startside, Grupper osv.)
+    "themeLight" = "#001b28"; 
+    "themeTertiary" = "#003650";
+    "themeSecondary" = "#005075";
+    "themeDarkAlt" = "#0e6891";
+    "themeDark" = "#277ba2";
+    "themeDarker" = "#5299ba";
     "neutralLighterAlt" = "#f8f8f8";
     "neutralLighter" = "#f4f4f4";
     "neutralLight" = "#eaeaea";
     "neutralQuaternaryAlt" = "#dadada";
     "neutralQuaternary" = "#d0d0d0";
     "neutralTertiaryAlt" = "#c8c8c8";
-    "neutralTertiary" = "#c2c2c2";
-    "neutralSecondary" = "#858585";
-    "neutralPrimaryAlt" = "#4b4b4b";
-    "neutralPrimary" = "#333333";
-    "neutralDark" = "#272727";
-    "black" = "#1d1d1d";
+    "neutralTertiary" = "#a2b9d8";
+    "neutralSecondary" = "#597eb1";
+    "neutralPrimaryAlt" = "#26518d";
+    "neutralPrimary" = "#15407d";
+    "neutralDark" = "#10315f";
+    "black" = "#0c2446";
     "white" = "#ffffff";
-    "primaryBackground" = "#ffffff";
-    "primaryText" = "#333333";
-    "bodyBackground" = "#ffffff";
-    "bodyText" = "#333333";
-    "disabledBackground" = "#f4f4f4";
-    "disabledText" = "#c8c8c8";
 }
 
-Add-SPOTheme -Identity "Lobe V1" -Palette $themeColors -IsInverted $false
+Add-SPOTheme -Identity "Lobe V2" -Palette $themeColors -IsInverted $false
